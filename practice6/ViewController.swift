d4225ce5bee65616992a7e889cf659dcc75bcfd7
Atsushi.Taketsuji <app@commude.ph>
2021-05-16 11:17:33 +0900

    Initial Commit

---
+++ practice6/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  practice6
+//
+//  Created by 竹辻篤志 on 2021/05/16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
